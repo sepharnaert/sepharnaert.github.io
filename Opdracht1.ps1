b8b45a23ccfa43bc0ccda257f44322bd04b098e6
Seph Arnaert <91336229+sepharnaert@users.noreply.github.com>
2022-02-21 21:30:27 +0000

    prutsen op VM

---
+++ Opdracht1.ps1
@@ -0,0 +1,23 @@
+﻿#Automatiseren installaties
+
+$host.PrivateData.ConsolePaneBackgroundColor = "Black"
+$host.PrivateData.ConsolePaneForegroundColor = "Green"
+$host.PrivateData.ConsolePaneTextBackgroundColor = "Black"
+$host.PrivateData.ErrorBackgroundColor = "Pink"
+$host.PrivateData.ErrorForegroundColor = "White"
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+Write-Host -BackgroundColor Black -ForegroundColor blue "Software voor Toegepaste Informatica"
+
+choco install -y adobereader vlc firefox  github-desktop git vscode
+
+
+Write-Host "Software voor System Engeneering Lab"
+
+choco install -y filezilla mysql virtualbox
+
+
+Write-Host -BackgroundColor Black -ForegroundColor Green "Gameprogrammas"
+
+choco install -y steam-client discord
